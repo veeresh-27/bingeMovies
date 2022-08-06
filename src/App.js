8b87398924e31608cec331aabf4723ad4eb22417
veeresh-27 <veereshrm789@gmail.com>
2022-08-06 23:03:35 +0530

    home oage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./pages/home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes,useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Login from "./pages/login";
 import Navbar from "./components/navbar";
 import Movies from "./pages/movies";
@@ -9,12 +10,21 @@ import Search from "./pages/search";
 import MovieInfo from "./pages/movieInfo";
 import Footer from "./components/footer";
 import MobileNavbar from "./components/mobileNav";
+import { useGoogleAuth } from "./context/GoogleAuth";
 
 function App() {
+  // let navigate = useNavigate();
+  // const { isSignedIn } = useGoogleAuth();
+  // useEffect(() => {
+  //   if (!isSignedIn) {
+  //     navigate("/");
+  //   }
+  // });
+
   return (
     <div className="App">
       <Router>
-        <Navbar />
+         <Navbar />
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
@@ -23,7 +33,7 @@ function App() {
           <Route path="/search" element={<Search />} />
           <Route path="/movieinfo/:entity/:id" element={<MovieInfo />} />
         </Routes>
-        <MobileNavbar/>
+         <MobileNavbar />
       </Router>
     </div>
   );
